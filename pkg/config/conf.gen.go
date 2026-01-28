428afa74c56905775241896e7c900901d92c34b6
Steve Gontzes <gontzess@gmail.com>
2026-01-27 19:16:45 -0500

    Upgrade Go to 1.25.2, baton-sdk to v0.7.4, update CI and golangci-lint v2 (#23)

--- pkg/config/conf.gen.go
+++ pkg/config/conf.gen.go
@@ -8,7 +8,7 @@ type Hubspot struct {
 	UserStatus bool `mapstructure:"user-status"`
 }
 
-func (c* Hubspot) findFieldByTag(tagValue string) (any, bool) {
+func (c *Hubspot) findFieldByTag(tagValue string) (any, bool) {
 	v := reflect.ValueOf(c).Elem() // Dereference pointer to struct
 	t := v.Type()
 
@@ -40,11 +40,13 @@ func (c *Hubspot) GetString(fieldName string) string {
 	if !ok {
 		return ""
 	}
-	t, ok := v.(string)
-	if !ok {
-		panic("wrong type")
+	if t, ok := v.(string); ok {
+		return t
 	}
-	return t
+	if t, ok := v.([]byte); ok {
+		return string(t)
+	}
+	panic("wrong type")
 }
 
 func (c *Hubspot) GetInt(fieldName string) int {
